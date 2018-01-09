9c3ac6ab65de88aec912616ba6241e859b6f99ea
martin byrne <marb61a@gmail.com>
2018-01-09 11:54:49 +0000

    added initial user model and navbar style

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {type: String, unique: true, default: ''},
+    fullname: {type: String, unique: true, default: ''},
+    email: {type: String, unique: true},
+    password: {type: String, default: ''},
+    userImage: {type: String, default: 'defaultPic.png'},
+    facebook: {type: String, default: ''},
+    fbTokens: Array,
+    google: {type: String, default: ''}
+    
+})
