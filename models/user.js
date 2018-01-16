efa210ee07f7d811bac0c444f8d97270a0d3fa37
martin byrne <marb61a@gmail.com>
2018-01-16 17:09:47 +0000

    updated models

--- models/user.js
+++ models/user.js
@@ -8,6 +8,21 @@ const userSchema = mongoose.Schema({
     userImage: {type: String, default: 'defaultPic.png'},
     facebook: {type: String, default: ''},
     fbTokens: Array,
-    google: {type: String, default: ''}
-    
-})
+    google: {type: String, default: ''},
+    sentRequest: [{
+        username: {type: String, default: ''}
+    }],
+    request:[{
+        userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        username: {type: String, default: ''}
+    }],
+    friendsList: [{
+        friendId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        friendName: {type: String, default: ''}
+    }],
+    totalRequest: {type: Number, default: 0},
+    gender: {type: String, default: ''},
+    country: {type: String, default: ''}
+});
+
+module.exports = mongoose.model('User', userSchema);
