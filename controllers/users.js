066863bd782aae8a4d9a8e1cdfbb2f9e05dea63a
martin byrne <marb61a@gmail.com>
2018-01-21 11:50:31 +0000

    updated users controller

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,39 @@ module.exports = function(_, passport, User){
     return {
         SetRouting: function(router){
             router.get('/', this.indexPage);
+            router.get('/signup', this.getSignUp);
+            router.get('/auth/facebook', this.getFacebookLogin);
+            router.get('/auth/facebook/callback', this.facebookLogin);
+            router.get('/auth/google', this.getGoogleLogin);
+            router.get('/auth/google/callback', this.googleLogin);
+            
+            
+            router.post('/', User.LoginValidation, this.postLogin);
+            router.post('/signup', User.SignUpValidation, this.postSignUp);
+            
         },
         
         indexPage: function(req, res){
             const errors = req.flash('error');
-        }
+            return res.render('index', {title: 'Sport Chat | Login', messages: errors, hasErrors: errors.length > 0});
+        },
+        
+        postLogin: passport.authenticate('local.login', {
+            successRedirect: '/home',
+            failureRedirect: '/',
+            failureFlash: true
+        }),
+        
+        getSignUp: function(req, res){
+            const errors = req.flash('error');
+            return res.render('signup', {title: 'Sport Chat | SignUp', messages: errors, hasErrors: errors.length > 0});
+        },
+        
+        postSignUp: passport.authenticate('local.signup', {
+            successRedirect: '/home',
+            failureRedirect: '/signup',
+            failureFlash: true
+        }),
+        
     }; 
 };
