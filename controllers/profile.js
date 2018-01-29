1ebcdb068486f9d05d83cc8bf7fdd0465ac9daa0
martin byrne <marb61a@gmail.com>
2018-01-29 12:19:30 +0000

    updated profile controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -18,6 +18,41 @@ module.exports = function(async, Users, Message, aws, formidable, FriendResult){
                         .exec((err, result) => {
                             callback(err, result);
                         });
+                },
+                function(callback){
+                    const nameRegex = new RegExp("^" + req.user.username.toLowerCase(), "i");
+                    Message.aggregate(
+                        {$match:{$or:[
+                            {"senderName":nameRegex},
+                            {"receiverName":nameRegex}
+                        ]}},
+                        {$sort:{"createdAt":-1}},
+                        {
+                            $group:{"_id":{
+                                "last_message_between":{
+                                    $cond:[
+                                        {
+                                            $gt:[
+                                            {$substr:["$senderName",0,1]},
+                                            {$substr:["$receiverName",0,1]}]
+                                        },
+                                        {$concat:["$senderName"," and ","$receiverName"]},
+                                        {$concat:["$receiverName"," and ","$senderName"]}
+                                    ]
+                                }
+                            }, "body": {$first:"$$ROOT"}
+                            }
+                        }, function(err, newResult){
+                            const arr = [
+                                {path: 'body.sender', model: 'User'},
+                                {path: 'body.receiver', model: 'User'}    
+                            ];
+                            
+                            Message.populate(newResult, arr, (err, newResult1) => {
+                                callback(err, newResult1);          
+                            });
+                        }
+                    );
                 }
             ]);
         }
