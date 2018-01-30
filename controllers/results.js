b95ca2c81bb885d1ea1888cd0d985a0b4c8ba3a9
martin byrne <marb61a@gmail.com>
2018-01-30 11:48:33 +0000

    updated results controller

--- controllers/results.js
+++ controllers/results.js
@@ -18,16 +18,46 @@ module.exports = function(async, Club, Users){
                     const regex = new RegExp((req.body.country), 'gi');
                 }
             ], (err, results) => {
+                const res1 = results[0];
                 
+                const dataChunk = [];
+                const chunkSize = 3;
+                
+                for(let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+                
+                res.render('results', {
+                    title: 'Sport Chat Results',
+                    user: req.user,
+                    chunks: dataChunk
+                });
             });
         },
         
         viewMembers: function(req, res){
             async.parallel([
                 function(callback){
-                    
+                    Users.find({}, (err, result) => {
+                        callback(err, result);
+                    });   
                 }    
-            ]);
+            ], (err, results) => {
+                const res1 = results[0];
+                
+                const dataChunk = [];
+                const chunkSize = 4;
+                
+                for(let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+                
+                res.render('members', {
+                    title: 'Sport Members',
+                    user: req.user,
+                    chunks: dataChunk
+                });
+            });
         }
     }; 
 };
