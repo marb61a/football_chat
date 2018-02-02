cd3569f72690bedd1c862c67135c4ec1d3e1280e
martin byrne <marb61a@gmail.com>
2018-02-02 12:16:14 +0000

    updated friendrequest helper

--- helpers/FriendResult.js
+++ helpers/FriendResult.js
@@ -18,6 +18,47 @@ module.exports = function(async, Users, Message){
                             callback(err, count);
                         });
                     }
+                },
+                
+                function(callback){
+                    if(req.body.receiverName){
+                        Users.update({
+                            'username': req.user.username,
+                            'sentRequest.username': {$ne: req.body.receiverName}
+                        }, 
+                        {
+                            $push: {sentRequest: {
+                                username: req.body.username
+                            }}
+                        }, (err, count) => {
+                            callback(err, count);
+                        });
+                    }
+                }
+            ], (err, results) => {
+                res.redirect(url);
+            });
+            
+            async.parallel([
+                function(callback){
+                    if(req.body.senderId){
+                        Users.update({
+                            '_id': req.user._id,
+                            'friendsList.friendId': {$ne: req.body.senderId}
+                        }, {
+                            $push: {friendsList: {
+                                friendId: req.body.senderId,
+                                friendName: req.body.senderName
+                            }},
+                            $pull: {request: {
+                                userId: req.body.senderId,
+                                username: req.body.senderName
+                            }},
+                            $inc: {totalRequest: -1}
+                        }, (err, count) => {
+                            callback(err, count);
+                        });
+                    }    
                 }    
             ]);
         }    
