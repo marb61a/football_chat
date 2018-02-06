ceafc5a9855856c2c966ff7d816ef27056f4321d
martin byrne <marb61a@gmail.com>
2018-02-06 11:44:30 +0000

    updated home controller

--- controllers/home.js
+++ controllers/home.js
@@ -8,7 +8,46 @@ module.exports = function(async, Club, _, Users, Message, FriendResult){
         },
         
         homePage: function(req, res){
-            
+            async.parallel([
+                function(callback){
+                    Club.find({}, (err, result) => {
+                        callback(err, result);
+                    });
+                },
+                
+                function(callback){
+                    Club.aggregate({
+                        $group:{
+                            _id: "$country"
+                        }
+                    }, (err, newResult) => {
+                        callback(err, newResult) ;
+                    });
+                },
+                
+                function(callback){
+                    Users.findOne({'username': req.user.username})
+                        .populate('request.userId')
+                        .exec((err, result) => {
+                            callback(err, result);
+                        });
+                },
+                
+                function(callback){
+                    const nameRegex = new RegExp("^" + req.user.username.toLowerCase(), "i");
+                    
+                    Message.aggregate(
+                        {$match:{$or:[
+                            {"senderName":nameRegex},
+                            {"receiverName":nameRegex}
+                        ]}},
+                        {$sort:{"createdAt":-1}},
+                        {
+                            
+                        }
+                    );
+                }
+            ]);
         }
     }; 
 };
