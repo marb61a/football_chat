a8aa3b7f12a79b112c972a2e8f167a63f0d0de79
martin byrne <marb61a@gmail.com>
2018-02-11 11:54:29 +0000

    updated index controller

--- controllers/home.js
+++ controllers/home.js
@@ -43,11 +43,46 @@ module.exports = function(async, Club, _, Users, Message, FriendResult){
                         ]}},
                         {$sort:{"createdAt":-1}},
                         {
+                            $group:{"_id": {
+                                "last_message_between": {
+                                    $cond: [
+                                        {
+                                            $gt: [
+                                                {$substr:["$senderName",0,1]},
+                                                {$substr:["$receiverName",0,1]}
+                                            ]
+                                        },
+                                        {$concat:["$senderName"," and ","$receiverName"]},
+                                        {$concat:["$receiverName"," and ","$senderName"]}
+                                    ]
+                                }
+                            }, "body": {$first:"$$ROOT"}
+                            }
+                        }, function(err, newResult){
+                            const arr = [
+                                {path: 'body.sender', model: 'User'},
+                                {path: 'body.receiver', model: 'User'}
+                            ];
                             
+                            Message.populate(newResult, arr, (err, newResult1) => {
+                                callback(err, newResult1);    
+                            });
                         }
                     );
                 }
-            ]);
+            ], (err, results) => {
+                const res1 = results[0];
+                const res2 = results[1];
+                const res3 = results[2];
+                const res4 = results[3];
+                
+                const dataChunk = {};
+                const chunkSize = 3;
+                
+                for(let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+            });
         }
     }; 
 };
