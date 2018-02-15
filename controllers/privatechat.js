89753830f26d76e8dd77e4b91967d9a153daa284
martin byrne <marb61a@gmail.com>
2018-02-15 11:53:27 +0000

    updated privatechat controller

--- controllers/privatechat.js
+++ controllers/privatechat.js
@@ -6,7 +6,41 @@ module.exports = function(async, Users, Message, FriendResult){
         },
         
         getchatPage: function(req, res){
-            
+            async.parallel([
+                function(callback){
+                    Users.findOne({'username':req.user.username})
+                        .populate('request.userId')
+                        .exec((err, result) => {
+                            callback(err, result);
+                        });
+                },
+                
+                function(callback){
+                    const nameRegex = new RegExp("^" + req.user.username.toLowerCase(), "i");
+                    
+                    Message.aggregate(
+                        {$match:{$or:[{"senderName":nameRegex}, {"receiverName":nameRegex}]}},
+                        {$sort:{"createdAt":-1}},
+                        {
+                            $group:{"_id":{
+                                "last_message_between": {
+                                    $cond:[
+                                        {
+                                            $gt:[
+                                                {$substr:["$senderName",0,1]},
+                                                {$substr:["$receiverName",0,1]}
+                                            ]
+                                        },
+                                        {$concat:["$senderName"," and ","$receiverName"]},
+                                        {$concat:["$receiverName"," and ","$senderName"]}
+                                    ]
+                                }
+                            }, "body": {$first:"$$ROOT"}
+                            }
+                        }
+                    )
+                }
+            ]);    
         }
     };  
 };
