f3c6bd50d5667c6f047d7cd1c990802ec68bd328
martin byrne <marb61a@gmail.com>
2018-02-20 11:54:30 +0000

    updated footballnews js file

--- public/js/news/footballnews.js
+++ public/js/news/footballnews.js
@@ -14,8 +14,28 @@ function GetResult(){
             $.each(data.response.results, function(i){
                 results += '<form class="paginate">';
                 results += '<div class="col-md-12 news-post">';
-                results += '<div class="row">'
+                results += '<div class="row">';
+                
+                results += '<a href='+data.response.results[i].webUrl+' target="_blank" style="color:#4aa1f3; text-decoration:none;">';
+                results += '<div class="col-md-2">';
+                results += '<img src='+data.response.results[i].fields.thumbnail+' class="img-responsive" />';
+                results += '</div>';
+                
+                results += '<div class="col-md-10">';
+                results += '<h4 class="news-date">'+new Date(Date.parse(data.response.results[i].webPublicationDate)).toDateString()+'</h4>';
+                results += '<h3>'+data.response.results[i].fields.headline+'</h3>';
+                results += '<p class="news-text">'+data.response.results[i].webTitle+'</p>';
+                results += '</div>';
+                
+                
+                results += '</a>';
+                results += '</div>';
+                results += '</div>';
+                results += '</form>';
             });
+            
+            $('#newsResults').html(results);
+            $('.paginate').slice(0, 2).show();
         }
     });
 }
@@ -25,5 +45,15 @@ function LoadData(divClass){
         e.preventDefault();
         
         $(divClass+":hidden").slice(0, 3).slideDown();
+        
+        $('html, body').animate({
+            scrollTop: $(this).offset().top
+        }, 2000);
+    });
+    
+    $('#linkTop').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
     });
 }
