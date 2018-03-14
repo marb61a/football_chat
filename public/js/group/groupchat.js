3fddc94baf8ef49e8b3e739ccc212abb34d8c7bf
martin byrne <marb61a@gmail.com>
2018-03-14 17:34:43 +0000

    updated groupchat.js

--- public/js/group/groupchat.js
+++ public/js/group/groupchat.js
@@ -31,5 +31,40 @@ $(document).ready(function(){
     socket.on('newMessage', function(data){
         var template = $('#message-template').html();
         
+        var message = Mustache.render(template, {
+            text: data.text,
+            sender: data.from,
+            userImage: data.image
+        });
+        
+        $('#messages').append(message);
     });
-})
+    
+    $('#message-form').on('submit', function(e){
+        e.preventDefault();
+        
+        var msg = $('#msg').val();
+        
+        socket.emit('createMessage', {
+            text: msg,
+            room: room,
+            sender: sender,
+            userPic: userPic
+        }, function(){
+            $('#msg').val('');
+        });
+        
+        $.ajax({
+            url: '/group/'+room,
+            type: 'POST',
+            data: {
+                message: msg,
+                groupName: room
+            },
+            success: function(){
+                $('#msg').val('');
+            }
+        });
+    });
+
+});
