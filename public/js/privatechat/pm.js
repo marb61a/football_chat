b3799bbcff3546b8288ff11e7379a7b9bb6de344
martin byrne <marb61a@gmail.com>
2018-03-18 12:09:30 +0000

    updated privatechat pm

--- public/js/privatechat/pm.js
+++ public/js/privatechat/pm.js
@@ -28,6 +28,36 @@ $(document).ready(function(){
     });
     
     socket.on('new message', function(data) {
+        var template = $('#message-template').html();
+        var message = Mustache.render(template, {
+            text: data.text,
+            sender: data.sender
+        });
         
+        $('#messages').append(message);
     });
+    
+    $('#message_form').on('submit', function(e){
+        e.preventDefault();
+        
+        var msg = $('#msg').val();
+        var sender = $('#name-user').val();
+        
+        if(msg.trim().length > 0){
+            socket.emit('private message', {
+                text: msg,
+                sender: sender,
+                room: paramOne
+            }, function(){
+                $('#msg').val('');
+            });
+        }
+    });
+    
 });
+
+function swap(input, value_1, value_2){
+    var temp = input[value_1];
+    input[value_1] = input[value_2];
+    input[value_2] = temp;
+}
